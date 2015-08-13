2a2bf997ac5e9fe6f12556dc216f6dfc688dd811
DevSusu <os1742@gmail.com>
2015-08-13 22:50:10 +0900

    change ruby version. update applicationcontroller. setup teamcontroller

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,10 @@
+class TeamsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def search
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
              }
 
   resources :users, only: [:index]
+  resources :teams, only: [:index,:new]
 end
