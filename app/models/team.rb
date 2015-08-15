7eb01c17604037305c19da0a9885d22faf6b785c
DevSusu <os1742@gmail.com>
2015-08-15 13:24:09 +0900

    fix css problem. fix team url validation

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,8 +2,8 @@ class Team < ActiveRecord::Base
   has_and_belongs_to_many :users
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
   validates :captain_user_id, presence: true, numericality: { only_integer: true }
-  validates :team_url, format: { with: /[-a-zA-Z0-9_]+/, message: "only type alphabet, numbers, _ and -" },
-             uniqueness: { message: "team url should be unique"}, length: { minimum: 4, maximum: 20 },
+  validates :team_url, format: { with: /[-a-z0-9_]+/, message: "only type lowercase alphabet, numbers, _ and -" },
+             uniqueness: { message: "another team has the url"}, length: { minimum: 4, maximum: 20 },
              exclusion: { in: %w(new edit search) }
 
   validates :gender, inclusion: { in: %w(Male Female), message: "gender should be male or female" }
