18170b958d766cb56ae867eb7619988ee3f7de4a
DevSusu <os1742@gmail.com>
2015-08-18 20:23:58 +0900

    add first name, last name for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:last_name, :first_name, :email, :password) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:last_name, :first_name, :email, :password, :current_password) }
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
 
   has_and_belongs_to_many :teams, :counter_cache => true
   has_many :applicants, dependent: :destroy
+
+  validates :name, length: { minimum: 2, maximum: 20 }
 end
