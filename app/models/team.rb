852a6c75183f36de7bc95712452c72c0d93530ee
DevSusu <os1742@gmail.com>
2015-08-18 19:39:15 +0900

    update wrong regex

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,8 +3,8 @@ class Team < ActiveRecord::Base
   has_many :applicants, dependent: :destroy
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
   validates :captain_user_id, presence: true, numericality: { only_integer: true }
-  validates :team_url, format: { with: /[-a-z0-9_]+/, message: "only type lowercase alphabet, numbers, _ and -" },
-             uniqueness: { message: "another team has the url"}, length: { minimum: 4, maximum: 20 },
+  validates :team_url, format: { with: /^[-a-z0-9_]{3,20}$/, message: "only type lowercase alphabet, numbers, _ and -" },
+             uniqueness: { message: "another team has the url"},
              exclusion: { in: %w(new edit search) }
 
   validates :gender, inclusion: { in: %w(Male Female), message: "gender should be male or female" }
