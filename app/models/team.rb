3249d5ffe168d7cfaa641faee32e8336c3902038
DevSusu <os1742@gmail.com>
2015-08-18 19:46:56 +0900

    fix regex anchors

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,7 +3,7 @@ class Team < ActiveRecord::Base
   has_many :applicants, dependent: :destroy
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
   validates :captain_user_id, presence: true, numericality: { only_integer: true }
-  validates :team_url, format: { with: /^[-a-z0-9_]{3,20}$/, message: "only type lowercase alphabet, numbers, _ and -" },
+  validates :team_url, format: { with: /\A[-a-z0-9_]{3,20}\Z/, message: "only type lowercase alphabet, numbers, _ and -" },
              uniqueness: { message: "another team has the url"},
              exclusion: { in: %w(new edit search) }
 
