2f194eb5d05743a69ca5a21c6baad2e1477200a7
DevSusu <os1742@gmail.com>
2015-08-20 16:08:38 +0900

    block users form destroying different applicants

--- app/controllers/user_infos_controller.rb
+++ app/controllers/user_infos_controller.rb
@@ -1,10 +1,13 @@
 class UserInfosController < ApplicationController
   def destroy
     user_info = UserInfo.find(params[:id])
-    applicant = Applicant.where(user: user_info.user.id, team: user_info.team.id).take
     
-    user_info.destroy!
-    applicant.destroy!
+    if user_info.user == current_user
+      applicant = Applicant.where(user: user_info.user.id, team: user_info.team.id).take
+
+      user_info.destroy!
+      applicant.destroy!
+    end
 
     redirect_to :back
   end
