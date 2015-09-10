ae251d474f0417ba10134eefef3aff632f28efc4
DevSusu <os1742@gmail.com>
2015-09-11 08:13:45 +0900

    users can view cups on their home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @teams = current_user.teams
+    @teams = current_user.all_teams
     @organizers = current_user.organizers
+
+    @cups = []
+    current_user.all_teams.each do |t|
+      @cups += t.team_applicants
+    end
   end
 end
