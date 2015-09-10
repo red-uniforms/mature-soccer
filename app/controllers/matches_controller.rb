5a789838266ad148cb8c68b7105c26fd0f21e977
DevSusu <os1742@gmail.com>
2015-09-11 07:22:34 +0900

    goals are written in top

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -11,6 +11,16 @@ class MatchesController < ApplicationController
     @players = @match.users
     @events = Event.where(match_id: @match.id)
     @event = @match.events.new
+
+    @home_goal = 0
+    @away_goal = 0
+    @events.each do |e|
+      if e.user.all_teams.include? @match.home_team
+        @home_goal += 1
+      elsif e.user.all_teams.include? @match.away_team
+        @away_goal += 1
+      end
+    end
   end
   def create
     date = DateTime.strptime(match_params[:date], "%Y-%m-%dT%H:%MT%Z")
